1bc19d32889dab257cb1fec28d2f07f7a3138474
Timo Stollenwerk <contact@timostollenwerk.net>
2012-03-01 12:40:18 +0100

    Renamed README.txt to .rst.

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ version = '1.0b1'
 setup(name='collective.js.formvalidation',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='',
+      keywords='plone zope javascript jquery form validation',
       author='Timo Stollenwerk',
       author_email='contact@timostollenwerk.net',
-      url='http://svn.plone.org/svn/collective/',
+      url='https://github.com/collective/collective.js.formvalidation',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.js'],
